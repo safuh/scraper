171d6d5143fe6f475c095a9fcdc0f9c6201f75de
safuh <harrysafu@gmail.com>
2023-09-07 02:52:03 +0300

    binaries

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 chmod +x geckodriver
+mv geckodriver /usr/local/bin/
+export PATH=$PATH:/usr/local/bin/geckodriver
+chmod +x /usr/local/bin/geckodriver
